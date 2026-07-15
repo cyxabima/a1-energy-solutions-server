c256e4f5763f44a03d7e58ce53def40501130c6d
cyxabima <ukashaanwerali@gmail.com>
2026-07-15 22:53:59 +0500

    endpoint connected to route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
 import { type Router as ExpressRouter, Router } from "express";
 import authRouter from "./auth.routes.js";
+import brandRouter from "./brand.routes.js";
 import categoryRouter from "./category.routes.js";
+import unitRouter from "./unit.routes.js";
 
 const router: ExpressRouter = Router();
 
 router.use("/auth", authRouter);
 router.use("/categories", categoryRouter);
+router.use("/brands", brandRouter);
+router.use("/units", unitRouter);
 
 export default router;
