6df362f4a97d3751e78046df2c1d1681b0a103d3
cyxabima <ukashaanwerali@gmail.com>
2026-07-15 13:34:50 +0500

    cors policy added cookie parser added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { type Express } from "express";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import router from "./router/index.js";
 
+const corsOptions = {
+	origin: [
+		"http://localhost:5173",
+		"https://hms-frontend-gray-eight.vercel.app",
+	],
+	methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+	credentials: true,
+};
+
 const app: Express = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/health", (_, res) => {
